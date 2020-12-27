736f2231fe1cb1ecb4710f56a856438a7b6d502b
Bagus Dewantoro <bagusdewantoro86@gmail.com>
2020-12-28 00:40:35 +0700

    admin

--- artikel/admin.py
+++ artikel/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product, Size, Type, StockLog
 
-# Register your models here.
+
+admin.site.register(Size)
+admin.site.register(Type)
+
+#===================================================================
+@admin.register(StockLog)
+class StockLogAdmin(admin.ModelAdmin):
+    list_display = ('product', 'production_date', 'quantity', 'sold', 'price')
+
+#===================================================================
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'product_size', 'figure1_preview', 'figure2_preview')
+
+    # tampilkan gambar di admin:
+    readonly_fields = ('figure1_preview', 'figure2_preview')
+
+    def figure1_preview(self, obj):
+        return obj.figure1_preview
+    figure1_preview.short_description = 'Image 1'
+    figure1_preview.allow_tags = True
+
+    def figure2_preview(self, obj):
+        return obj.figure2_preview
+    figure2_preview.short_description = 'Image 2'
+    figure2_preview.allow_tags = True
--- artikel/models.py
+++ artikel/models.py
@@ -3,38 +3,6 @@ from django.urls import reverse
 from django.utils.html import mark_safe # menampilkan gambar di admin
 import uuid # untuk generate id
 
-#=====================================================================
-class Product(models.Model):
-    name = models.CharField(max_length=20)
-    description = models.TextField(max_length=100, help_text='(max = 100 character)')
-    figure1 = ImageField(upload_to='static/img', null=True, blank=True)
-    figure2 = ImageField(upload_to='static/img', null=True, blank=True)
-    size = models.ManyToManyField(Size)
-    type = models.ForeignKey('Type', on_delete=models.SET_NULL, null=True)
-
-    # tampilkan gambar di admin:
-    @property
-    def figure1_preview(self):
-        if self.figure1:
-            return mark_safe('<img src="{}" width="auto" height="100" />'.format(self.figure1.url))
-        return ''
-
-    @property
-    def figure2_preview(self):
-        if self.figure2:
-            return mark_safe('<img src="{}" width="auto" height="100" />'.format(self.figure2.url))
-        return ''
-
-    class Meta:
-        ordering = ['name']
-        verbose_name = 'Name'
-        verbose_name_plural = 'Name'
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse('detail-product', args=[str(self.id)])
 
 #=====================================================================
 class Size(models.Model):
@@ -61,7 +29,46 @@ class Type(models.Model):
         return self.type_name
 
 #=====================================================================
-class InstanceProduct(models.Model):
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=100, help_text='(max = 100 character)')
+    figure1 = models.ImageField(upload_to='static/img', null=True, blank=True)
+    figure2 = models.ImageField(upload_to='static/img', null=True, blank=True)
+    size = models.ManyToManyField(Size)
+    type = models.ForeignKey('Type', on_delete=models.SET_NULL, null=True)
+
+    # tampilkan gambar di admin:
+    @property
+    def figure1_preview(self):
+        if self.figure1:
+            return mark_safe('<img src="{}" width="auto" height="100" />'.format(self.figure1.url))
+        return ''
+
+    @property
+    def figure2_preview(self):
+        if self.figure2:
+            return mark_safe('<img src="{}" width="auto" height="100" />'.format(self.figure2.url))
+        return ''
+
+    class Meta:
+        ordering = ['name', 'type']
+        verbose_name = 'Name'
+        verbose_name_plural = 'Name'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail-product', args=[str(self.id)])
+
+    # tambahan method untuk ManyToManyField = size
+    def product_size(self):
+        return ', '.join(size.size_name for size in self.size.all()[:5])
+
+    product_size.short_description = 'Size'
+
+#=====================================================================
+class StockLog(models.Model):
     id = models.UUIDField('Code', primary_key=True, default=uuid.uuid4)
     product = models.ForeignKey('Product', verbose_name='Name', on_delete=models.SET_NULL, null=True)
     production_date = models.DateField(null=True, blank=True)
@@ -72,12 +79,9 @@ class InstanceProduct(models.Model):
     # setelah dapet di atas, bikin perkalian antara sold dan quantity
 
     class Meta:
-        ordering = ['production_date']
+        ordering = ['production_date', 'product', 'quantity', 'price', 'sold']
         verbose_name = 'Stock Log'
         verbose_name_plural = 'Stock Log'
 
     def __str__(self):
-        return f'{self.id} ({self.product.name})'
-
-#=====================================================================
-    
+        return f'{self.product.name} ({self.id})'
